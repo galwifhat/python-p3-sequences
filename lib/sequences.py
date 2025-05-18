9e7b1a079efb241c9faeaaa747a48e2bba24249a
Ruth <cirurue@gmail.com>
2025-05-18 22:39:06 +0300

    range fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    # Handle edge cases for length 0, 1, or 2
+    # Handle edge cases for length 0, or 1
     if length <= 0:
         fibonacci_sequence = []
     elif length == 1:
@@ -18,3 +18,18 @@ def print_fibonacci(length):
     return fibonacci_sequence
 
 print(print_fibonacci(10)) # first 10 Fibonacci nums
+
+
+def print_fibonacci2(length):
+    if length <= 0:
+        fib_sequence = []
+    elif length == 1:
+        fib_sequence = [0]
+    else:
+        fib_sequence = [0, 1]
+        for _ in range(length-2): 
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+    print(fib_sequence)
+    return fib_sequence
+print(print_fibonacci2(3))
